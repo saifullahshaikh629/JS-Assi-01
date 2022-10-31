bd249fe44e27c47c9e75f17ab6c4ee5d9b6123c3
saifullah <shaikhsaifullah629@gmail.com>
2022-10-31 05:07:10 -0700

    JS 01

---
+++ app.js
@@ -0,0 +1,41 @@
+// alert("Thanks For Comming This page")
+// window.alert("Thanks For Comming This page");
+
+//  var a = 30;
+//  var b = 20;
+//  var c = a + b;
+// alert(c);
+
+// var a = 30;
+//  var b = 20;
+//  var c = a - b;
+// alert(c);
+
+// var a = 30;
+//  var b = 20;
+//  var c = a * b;
+// alert(c);
+
+// var a = 30;
+//  var b = 20;
+//  var c = a / b;
+// alert(c);
+
+// var a = 30;
+//  var b = 10;
+//  var c = 5;
+//  var d = (a + b) - c;
+// alert(d);
+
+
+// var a = 23;
+// var c = a + 7;
+// alert(c);
+
+
+// var a = 50;
+// var b = 40;
+// a++;
+// b--;
+// console.log(a);
+// console.log(b);
